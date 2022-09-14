7ad96bf4b04245894480190c2fe730d6cc75725d
Philip Okanlawon <f.bobo@live.com>
2022-09-13 23:10:02 -0600

    complete project

--- script.js
+++ script.js
@@ -5,6 +5,43 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const namItems = [nav1, nav2, nav3, nav4, nav5];
+
+toggleNav = () => {
+  // Toggle: Menu Bars Open/Closed
+  menuBars.classList.toggle('change');
+  // Toggle: Menu Active
+  overlay.classList.toggle('overlay-active');
+  if (overlay.classList.contains('overlay-active')) {
+    // Animate In - Overlay
+    overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+    // Animate In - Nav Items
+    nav1.classList.remove('slide-out-1');
+    nav1.classList.add('slide-in-1');
+    nav2.classList.remove('slide-out-2');
+    nav2.classList.add('slide-in-2');
+    nav3.classList.remove('slide-out-3');
+    nav3.classList.add('slide-in-3');
+    nav4.classList.remove('slide-out-4');
+    nav4.classList.add('slide-in-4');
+    nav5.classList.remove('slide-out-5');
+    nav5.classList.add('slide-in-5');
+  } else {
+    // Animate Out - Overlay
+    overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+    // Animate In - Nav Items
+    nav1.classList.remove('slide-in-1');
+    nav1.classList.add('slide-out-1');
+    nav2.classList.remove('slide-in-2');
+    nav2.classList.add('slide-out-2');
+    nav3.classList.remove('slide-in-3');
+    nav3.classList.add('slide-out-3');
+    nav4.classList.remove('slide-in-4');
+    nav4.classList.add('slide-out-4');
+    nav5.classList.remove('slide-in-5');
+    nav5.classList.add('slide-out-5');
+  }
+}
 
 // Event Listeners
 menuBars.addEventListener('click', toggleNav);
